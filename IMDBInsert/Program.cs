48c80554d02bb4864343bd4ce25bcfbdfabc87c3
move <move@zealand.dk>
2023-02-24 14:51:16 +0100

    Add project files.

---
+++ IMDBInsert/Program.cs
@@ -0,0 +1,22 @@
+﻿using IMDBInsert;
+using System.Data.SqlClient;
+
+DateTime before = DateTime.Now;
+List<Title> titles = new List<Title>();
+foreach (string line in System.IO.File.ReadLines(@"C:\Users\zealand\Documents\lektor\title.basics.tsv")
+    .Skip(1).Take(10000))
+{
+    string[] values = line.Split('\t');
+    if (values.Length != 9)
+    {
+        titles.Add(new Title() { tconst = values[0],
+            primaryTitle = values[2],
+            originalTitle= values[3] });
+    }
+}
+DateTime after = DateTime.Now;
+
+Console.WriteLine(after - before);
+
+SqlConnection sqlConn = new SqlConnection(
+    "server=localhost;database=IMDB,User ID=imdbuser;password=superSecret");
---
+++ IMDBInsert/Title.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBInsert
+{
+    public class Title
+    {
+        public string tconst { get; set; }
+        public string primaryTitle { get; set; }
+        public string originalTitle { get; set; }
+    }
+}

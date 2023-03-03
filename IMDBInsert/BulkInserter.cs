90b628e85272c992a59db0d36c26492f5c81a2e3
move <move@zealand.dk>
2023-03-03 13:40:29 +0100

    added bulk

---
+++ IMDBInsert/BulkInserter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBInsert
+{
+    public class BulkInserter : IInserter
+    {
+        public void InsertTitles(SqlConnection sqlConn, List<Title> allTitles)
+        {
+            DataTable titleTable = new DataTable("Titles");
+
+            titleTable.Columns.Add("tconst", typeof(string));
+            titleTable.Columns.Add("primaryTitle", typeof(string));
+            titleTable.Columns.Add("originalTitle", typeof(string));
+            titleTable.Columns.Add("startYear", typeof(int));
+            titleTable.Columns.Add("endYear", typeof(int));
+
+            foreach (Title title in allTitles) { 
+                DataRow titleRow = titleTable.NewRow();
+                titleRow["tconst"] = title.tconst;
+                titleRow["primaryTitle"] = title.primaryTitle;
+                titleRow["originalTitle"] = title.originalTitle;
+                if (title.startYear == null)
+                {
+                    titleRow["startYear"] = DBNull.Value;
+                }
+                else
+                {
+                    titleRow["startYear"] = title.startYear;
+                }
+                if (title.endYear == null)
+                {
+                    titleRow["endYear"] = DBNull.Value;
+                } else
+                {
+                    titleRow["endYear"] = title.endYear;
+                }
+
+                titleTable.Rows.Add(titleRow);
+            }
+            SqlBulkCopy bulkCopy = new SqlBulkCopy(sqlConn, 
+                SqlBulkCopyOptions.KeepNulls,null);
+            bulkCopy.DestinationTableName = "Titles";
+            bulkCopy.BulkCopyTimeout = 0;
+            bulkCopy.WriteToServer(titleTable);
+        }
+    }
+}
--- IMDBInsert/Program.cs
+++ IMDBInsert/Program.cs
@@ -1,35 +1,28 @@
 ﻿using IMDBInsert;
 using System.Data.SqlClient;
 
-Console.WriteLine("Press 1 for delete, 2 for normal insert, 3 for prepared insert");
+Console.WriteLine("Press 1 for delete, 2 for normal insert, " +
+    "3 for prepared insert");
+Console.WriteLine("5 for bulk insert");
 string action = Console.ReadLine();
 
 
 DateTime before = DateTime.Now;
 List<Title> titles = new List<Title>();
 foreach (string line in System.IO.File.ReadLines(@"C:\temp\title.basics.tsv\data.tsv")
-    .Skip(1).Take(10000))
+    .Skip(1))
 {
     string[] values = line.Split('\t');
     if (values.Length == 9)
     {
-        int endYear = 0;
-        bool canParse = int.TryParse(values[6], out endYear);
-        int? actualEndYear = null;
-        if (endYear != 0)
-        {
-            actualEndYear = endYear;
-        }
-
         titles.Add(new Title()
         {
             tconst = values[0],
             primaryTitle = values[2],
             originalTitle = values[3],
-            startYear = int.Parse(values[5]),
-            endYear = actualEndYear
+            startYear = CheckIntColumn(values[5]),
+            endYear = CheckIntColumn(values[6])
         });
-
     }
 }
 DateTime after = DateTime.Now;
@@ -41,7 +34,7 @@ Console.WriteLine("Amount of titles: " + titles.Count());
 before = DateTime.Now;
 
 using (SqlConnection sqlConn = new SqlConnection(
-    "server=localhost;database=IMDB;User ID=imdbuser;password=superSecret"))
+    "server=localhost;database=IMDB;User ID=imdbuser;password=superSecret;"))
 {
     sqlConn.Open();
 
@@ -58,8 +51,11 @@ using (SqlConnection sqlConn = new SqlConnection(
             IInserter inserter2 = new PreparedInserter();
             inserter2.InsertTitles(sqlConn, titles);
             break;
+        case "5":
+            IInserter inserter3 = new BulkInserter();
+            inserter3.InsertTitles(sqlConn, titles);
+            break;
     }
-    DeleteTitles(sqlConn);
 }
 after = DateTime.Now;
 
@@ -69,4 +65,16 @@ void DeleteTitles(SqlConnection sqlConn)
 {
     SqlCommand sqlComm = new SqlCommand("DELETE FROM Titles", sqlConn);
     sqlComm.ExecuteNonQuery();
+}
+
+int? CheckIntColumn(string value)
+{
+    int beforeParse = 0;
+    bool canParse = int.TryParse(value, out beforeParse);
+    int? result = null;
+    if (beforeParse != 0)
+    {
+        result = beforeParse;
+    }
+    return result;
 }
